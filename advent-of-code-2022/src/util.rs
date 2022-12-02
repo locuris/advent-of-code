7941f1a1f12951e3669b97f2f2594b3c2b9cdfe7
Louis Kolodziej <louis.kolodziej@fundapps.co>
2022-12-02 21:30:29 +0000

    fixed path reference

--- advent-of-code-2022/src/util.rs
+++ advent-of-code-2022/src/util.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-const FILEPATH: &str = "Data/";
+const FILEPATH: &str = "data/";
 
 pub fn parse_input(filename: &str) -> String {
     let result = fs::read_to_string(String::from(FILEPATH) + filename).expect("Could not read file.");
