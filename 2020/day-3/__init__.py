70672921f49a5a64cb788b9d4e8914cfdc82deba
Louis Kolodziej <louis92ck@gmail.com>
2023-03-21 19:20:23 +0000

    Added part ii logic
    Additional Authors: Oliver

--- 2020/day-3/__init__.py
+++ 2020/day-3/__init__.py
@@ -2,40 +2,58 @@ from common.grid_objects import Point
 from common.data import as_lines
 
 
-def part_i():
-    test_input = as_lines(False)
-    height = len(test_input)
-    area = {}
-    width = 0
-    answer = 1
+width = 0
+height = 0
 
+def ride_the_slope(slope: Point, area: dict[Point, bool]) -> int:
+    trees = 0
+    current_point = Point(0, 0)
+    while current_point.y < height:
+        current_point + slope
+        if current_point.y >= height:
+            break
+        if current_point.x >= width:
+            new_x_position = current_point.x - width
+            current_point = Point(new_x_position, current_point.y)
+        if area[current_point]:
+            trees += 1
+    return trees
+
+
+def prepare_area() -> dict[Point, bool]:
+    input_data = as_lines(False)
+    global height
+    height = len(input_data)
+    global width
+    width = 0
+    area = {}
+    for y, line in enumerate(input_data):
+        if width == 0:
+            width = len(line)
+        for x, char in enumerate(line):
+            point = Point(x, y)
+            area[point] = True if char == '#' else False
+    return area
+
+
+def part_i():
+    area = prepare_area()
+    answer = ride_the_slope(Point(3,1), area)
+    print(f"Part I: {answer}")
+
+
+def part_ii():
+    area = prepare_area()
     slopes = [Point(1, 1), Point(3, 1),
               Point(5, 1), Point(7, 1), Point(1, 2)]
+    answer = 1
 
     for slope in slopes:
-        for y, line in enumerate(test_input):
-            if width == 0:
-                width = len(line)
-            for x, char in enumerate(line):
-                point = Point(x, y)
-                area[point] = True if char == '#' else False
+        answer *= ride_the_slope(slope, area)
 
-        trees = 0
-        current_point = Point(0, 0)
-        while current_point.y < height:
-            current_point + slope
-            if current_point.y >= height:
-                break
-            if current_point.x >= width:
-                new_x_position = current_point.x - width
-                current_point = Point(new_x_position, current_point.y)
-            if area[current_point]:
-                trees += 1
-        answer *= trees
-
-        print(trees)
-    print("answer is " + str(answer))
+    print(f"Part II: {answer}")
 
 
 if __name__ == '__main__':
     part_i()
+    part_ii()
