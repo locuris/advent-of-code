6515117549569dd69892699a46ace2602a47de90
Louis Kolodziej <louis.kolodziej@fundapps.co>
2023-08-11 15:26:37 +0100

    wip

--- f#/AoC-2020/Day13.fs
+++ f#/AoC-2020/Day13.fs
@@ -11,4 +11,28 @@ let day13part1 (lines: string array) : string =
     bus * wait |> string
     
 let day13part2 (lines: string array) : string =
-    "NOT DONE"
+    let _, busses = prepareNotes lines
+    
+    let busMap = busses |> Array.mapi (fun i bt -> (bt, i)) |> Array.filter (fun (bt, _) -> bt <> 1) |> Map.ofArray
+    let busesSorted = busses |> Array.sortDescending
+    
+    let mutable found = false
+    let mutable t = 0
+    while not found do
+        t <- t + 1
+        let longestBus = busesSorted.[0]
+        let longestBusTime = longestBus * t
+        let longestBusIndex = busMap.[longestBus]
+        found <- true
+        
+        for i = 1 to busesSorted.Length - 1 do
+            let bus = busesSorted.[i]
+            let busIndex = busMap.[bus]
+            let diff = longestBusIndex + busIndex
+            let busTime = longestBusTime - diff
+            if found && busTime % bus <> 0 then
+                found <- false
+                t <- t + 1
+            
+    busses.[0] * t |> string
+            
